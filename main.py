339520e06094819fe3ffb912542b6b0ba6b648ee
jenny-rcd <jennyrichard72000@gmail.com>
2024-03-02 19:07:01 +0100

    update

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = FastAPI() # Initialization d'une api FastAPI
 
 app.include_router(userRouter, prefix="/user") # Création d'un groupe de route avec comme prefix "user" donc -> "http://localhost:7676/user/..."
 
+app.include_router()
+
 # code pour pouvoir lancer l'api avec py et non uvicorn
 if __name__ == "__main__":
     import uvicorn # Importation du serveur uvicorn
